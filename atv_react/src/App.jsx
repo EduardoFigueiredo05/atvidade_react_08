16daa4b4b0a399d9d3da5c57df66aaf32dfb0ab9
Eduardo Figueiredo <eduguesfigueiredo@gmail.com>
2026-09-08 21:55:16 -0300

    Terceiro commit

--- atv_react/src/App.jsx
+++ atv_react/src/App.jsx
@@ -1,35 +1,36 @@
-
-import ReactLogo from './assets/logo_react.png'
-import './styles.css'
+import ReactLogo from "./assets/logo_react.png";
+import "./styles.css";
+import { useState } from "react";
 
 const content = [
   [
     "React is extremely popular",
     "It makes building complex, interactive UIs a breeze",
     "It's powerful & flexible",
-    "It has a very active and versatile ecosystem"
+    "It has a very active and versatile ecosystem",
   ],
   [
     "Components, JSX & Props",
     "State",
     "Hooks (e.g., useEffect())",
-    "Dynamic rendering"
+    "Dynamic rendering",
   ],
   [
     "Official web page (react.dev)",
     "Next.js (Fullstack framework)",
-    "React Native (build native mobile apps with React)"
+    "React Native (build native mobile apps with React)",
   ],
   [
     "Vanilla JavaScript requires imperative programming",
     "Imperative Programming: You define all the steps needed to achieve a result",
     "React on the other hand embraces declarative programming",
-    "With React, you define the goal and React figures out how to get there"
-  ]
+    "With React, you define the goal and React figures out how to get there",
+  ],
 ];
 
 function App() {
-      return (
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
+  return (
     <div>
       <header>
         <img src={ReactLogo} alt="React logo" />
@@ -39,19 +40,24 @@ function App() {
         </div>
       </header>
 
-  <div id="tabs">
+      <div id="tabs">
         <menu>
-          <button>Why React?</button>
-          <button>Core Features</button>
-          <button>Related Resources</button>
+          <button onClick={() => setActiveContentIndex(0)}>Why React?</button>
+
+          <button onClick={() => setActiveContentIndex(1)}>
+            Core Features
+          </button>
+
+          <button onClick={() => setActiveContentIndex(2)}>
+            Related Resources
+          </button>
         </menu>
 
         <div id="tab-content">
           <ul>
-            <li>React is extremely popular</li>
-            <li>It makes building complex, interactive UIs a breeze</li>
-            <li>It's powerful & flexible</li>
-            <li>It has a very active and versatile ecosystem</li>
+            {content[activeContentIndex].map((item) => (
+              <li key={item}>{item}</li>
+            ))}
           </ul>
         </div>
       </div>
