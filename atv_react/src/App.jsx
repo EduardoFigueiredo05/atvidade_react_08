7973c05c29b99fee3c40e3605e43b0a4265f1bf7
Eduardo Figueiredo <eduguesfigueiredo@gmail.com>
2026-09-08 21:27:05 -0300

    Vetores criados

--- atv_react/src/App.jsx
+++ atv_react/src/App.jsx
@@ -1,5 +1,32 @@
 
-import ReactLogo from './logo_react.png'
+import ReactLogo from './assets/logo_react.png'
+import './styles.css'
+
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering"
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)"
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there"
+  ]
+];
 
 function App() {
       return (
@@ -32,4 +59,4 @@ function App() {
   );
 }
 
-export default App ();
+export default App;
